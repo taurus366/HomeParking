c0312c4c203777fc7ed637be01c36b0c57ca807d
start.vaadin.com <no-reply@vaadin.com>
2023-07-30 11:22:57 +0000

    Generated project

---
+++ src/main/java/com/parking/www/data/Role.java
@@ -0,0 +1,5 @@
+package com.parking.www.data;
+
+public enum Role {
+    USER, ADMIN;
+}
---
+++ src/main/java/com/parking/www/data/service/UserRepository.java
@@ -0,0 +1,10 @@
+package com.parking.www.data.service;
+
+import com.parking.www.data.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
+
+    User findByUsername(String username);
+}
